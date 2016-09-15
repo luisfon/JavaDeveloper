3a5e8ad776fed63c96ef6518a9116a132ede2261
luisfon <ramirezfonsecaluisantonio@hotmail.com>
2016-09-15 01:49:38 -0500

    casting

---
+++ basicos/Cast.java
@@ -0,0 +1,21 @@
+package poo;
+
+public class Cast {
+
+	public static void main(String[] args)
+	{
+		
+		
+		//almacenar tipo de dato double en un entero
+		
+		double x = 9.22;
+		
+		System.out.println(x);
+		
+		int y = (int)x;
+
+		System.out.println(y);
+		
+	}
+
+}
